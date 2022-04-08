9cc260e730f783a4cb03c56d2e3285e7b80ce4ff
roblesisaac <irobles1030@gmail.com>
2022-04-08 15:14:48 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -5,23 +5,12 @@ const { Chain, convert, obj, type } = require("./scripts/chain");
 const handle = new Chain({
   steps: {
     respond: function(event) {
-      var a = {
-        name: {
-          first: "isaac"
-        }
-      };
-      
-      [1,2,3].loop((i, item , nx) => {
-        a.name.first += item;
-        nx();
-      }).then(() => {
         this.next({
           test,
           name: obj.deep(a, "name.first"),
           message: "chain is running!",
           event
         });
-      });
     }
   },
   instruct: {
@@ -29,8 +18,6 @@ const handle = new Chain({
   }
 });
 
-module.exports.port = async (event) => {
-	return handle.serve(event)
+module.exports.port = async event => handle.serve(event)
                 .then(response => response)
                 .catch(e => e);
-}
