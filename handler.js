34e9a260f580be198f6253a8b742c87e0b96775b
roblesisaac <irobles1030@gmail.com>
2022-04-08 13:08:21 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -5,5 +5,5 @@ import Chain from "/srcipts/chain.js";
 module.exports.port = async (event) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  return { message: 'Go Serverless v3.0! Your function executed successfully!', event };
+  return { message: 'Go Serverless v30.0! Your function executed successfully!', event };
 };
