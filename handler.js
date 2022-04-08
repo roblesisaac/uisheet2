ff1c16b145baf43404bda2dc0ab801e1597ca914
roblesisaac <irobles1030@gmail.com>
2022-04-08 13:03:22 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -3,5 +3,5 @@
 module.exports.port = async (event) => {
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  return { message: 'Go Serverless v3.0! Your function executed successfully!', event };
 };
