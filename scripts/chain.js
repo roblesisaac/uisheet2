86918d0d96391efc2fef61f212b14817c3ae325e
roblesisaac <irobles1030@gmail.com>
2022-04-08 17:11:49 -0700

    Update chain.js

--- scripts/chain.js
+++ scripts/chain.js
@@ -1,253 +1,3 @@
-//utils
-var convert = {
-    toArray: (data) => {
-        return Array.isArray(data) ?
-            data :
-            [data];
-    },
-    toInstruct: (steps) => {
-        return Array.isArray(steps) ?
-            steps :
-            typeof steps == "function" ?
-            steps() :
-            [steps];
-    },
-    toObject: (data, caller) => {
-        caller = caller || this;
-        if (typeof data == "function") data = data.call(caller);
-        return typeof data == "object" && !Array.isArray(data) ? data : {
-            data
-        };
-    }
-};
-
-var addMethodTo = (type, name, value) => {
-    var proto = type.prototype;
-    if (!proto || proto[name]) return;
-    Object.defineProperty(proto, name, {
-        value,
-        enumerable: false,
-        writable: true
-    });
-};
-
-addMethodTo(Array, "loop", function(method, i = 0) {
-    var next = () => {
-            this.loop(method, i + 1);
-        },
-        finish = (promise) => {
-            promise = promise || this._promise;
-
-            if (promise) promise.call(this);
-        };
-
-    if (i == this.length) {
-        finish();
-        return {
-            then: finish
-        };
-    }
-
-    method(i, this[i], function() {
-        setTimeout(next);
-    });
-
-    return {
-        then: (promise) => {
-            this._promise = promise;
-        }
-    };
-});
-addMethodTo(Array, "excludes", function(keyword) {
-    return this.indexOf(keyword) == -1;
-});
-addMethodTo(Array, "includes", function(keyword) {
-    return this.indexOf(keyword) > -1;
-});
-addMethodTo(Array, "find", function(filter) {
-
-});
-
-var obj = {
-    copy: function(object) {
-        if (null === object || typeof object != "object") return object;
-        var copy = object.constructor();
-        for (var attr in object) {
-            if (object.hasOwnProperty(attr)) copy[attr] = object[attr];
-        }
-        return copy;
-    },
-    assignNative: function(obj, prop, value) {
-        Object.defineProperty(obj, prop, {
-            enumerable: false,
-            writable: true,
-            value
-        });
-    },
-    deep: function(object, props, sliced) {
-        if (!object || !props) return;
-        
-        if(!sliced) {
-            if (typeof props == "string") props = props.split(".");
-            if(!props.slice) return props;
-            sliced = props.slice();   
-        }
-
-        var nested = object[sliced.shift()];
-
-        return sliced.length ?
-            this.deep(nested, props, sliced) :
-            nested;
-    },
-    hasProp: function(object, prop) {
-        return !object ?
-            false :
-            Object.keys(object).includes(prop) || !!object[prop];
-    },
-    watch: function(object, prop, fn) {
-        var og = object[prop];
-        Object.defineProperty(object, prop, {
-            get: () => og,
-            set: function(value) {
-                if (fn) fn(value);
-                og = value;
-            }
-        });
-    },
-    valueTip: function(obj, props, sliced) {
-        if (!obj) return;
-
-        if (!Array.isArray(props)) props = props.split(".");
-
-        var arrProps = sliced ? props : props.slice(),
-            nested = arrProps.length > 1 ? obj[arrProps.shift()] : obj;
-
-        return arrProps.length > 1 ?
-            this.valueTip(nested, arrProps, true) :
-            {
-                obj: nested,
-                prop: arrProps[0]
-            };
-    }
-};
-
-var type = {
-    isNotObject: obj => Array.isArray(obj) || typeof obj != "object",
-    isObject: obj => obj && !Array.isArray(obj) && typeof obj == "object"
-};
-
-addMethodTo(String, "includes", function() {
-    "use strict";
-    return String.prototype.indexOf.apply(this, arguments) != -1;
-});
-addMethodTo(String, "excludes", function() {
-    "use strict";
-    return String.prototype.indexOf.apply(this, arguments) == -1;
-});
-addMethodTo(String, "matchCount", function() {
-    var str = this.valueOf(),
-        matches = 0;
-
-    for (var i in arguments) {
-        var value = arguments[i];
-        if (str.includes(value)) matches++;
-    }
-
-    return matches;
-});
-addMethodTo(String, "excludesAll", function() {
-    var matchCount = this.matchCount.apply(this, arguments);
-
-    return !matchCount;
-});
-addMethodTo(String, "includesAll", function() {
-    var args = arguments,
-        matchCount = this.matchCount.apply(this, args);
-
-    return matchCount == args.length;
-});
-addMethodTo(String, "includesAny", function() {
-    var matchCount = this.matchCount.apply(this, arguments);
-
-    return !!matchCount;
-});
-
-//end utils ---
-function Memory(chain) {
-    var bp = chain._blueprint,
-        format = data => obj.copy(convert.toObject(data || {}, chain));
-
-    var assignProps = (assignee, dataObj) => {
-        if (!dataObj) return;
-
-        var define = (prop, definer) => {
-                Object.defineProperty(assignee, prop, definer);
-            },
-            defineGetterMethod = (value, prop) => {
-                define(prop, {
-                    enumerable: true,
-                    get: value.bind(this)
-                });
-            },
-            getAndSetFromChain = (prop) => {
-                define(prop, {
-                    enumerable: true,
-                    get: () => chain[prop],
-                    set: (newValue) => chain[prop] = newValue
-                });
-            };
-
-        var keys = Object.keys,
-            data = format(dataObj),
-            assignProp = (prop) => {
-                var value = data[prop];
-
-                if (keys(assignee).includes(prop)) return;
-
-                if (keys(chain).includes(prop)) {
-                    getAndSetFromChain(prop);
-                    return;
-                }
-
-                if (typeof value == "function") {
-                    defineGetterMethod(value, prop);
-                    return;
-                }
-
-                assignee[prop] = value;
-            };
-
-        keys(data).forEach(assignProp);
-    };
-
-    assignProps(chain, bp.state);
-    assignProps(this, bp.state);
-    assignProps(this, bp.input);
-}
-
-Memory.prototype._remember = function() {
-    for (var i in arguments) {
-        Object.assign(this, arguments[i]);
-    }
-    return this;
-};
-
-Memory.prototype._addTools = function(data) {
-    var config = (prop) => {
-        return {
-            configurable: true,
-            writable: true,
-            value: data[prop]
-        };
-    };
-
-    for (var prop in data) {
-        Object.defineProperty(this, prop, config(prop));
-    }
-
-    return this;
-}
-
 function Chain(blueprint) {
     var instruct = blueprint.instruct;
 
@@ -374,6 +124,10 @@ Chain.prototype._library = {
             setTimeout(next, time * 1000);
         }
     }
+}
+
+Chain.prototype.addGlobalSteps = function(steps) {
+	Object.assign(Chain.prototype._library.steps, steps);
 }
 
 function buildChain(stepsArr, chain, chainName) {
@@ -606,7 +360,15 @@ function buildSteps(stepsArr, chain, chainName, prev, stepIndex, specialProp) {
             }
 
             if (typeof method != "function") {
-                memory._remember(stepData());
+								var data = stepData();
+
+								for(var key in data) {
+									var value = data[key];
+
+									data[key] = obj.deep(memory, value) || value;
+								}
+
+                memory._remember(data);
                 return next();
             }
 
