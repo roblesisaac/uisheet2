bb405884ccf82f1bc2fc090660bb27454131d66c
roblesisaac <irobles1030@gmail.com>
2022-04-12 16:23:21 -0700

    Update chain.js

--- scripts/chain.js
+++ scripts/chain.js
@@ -1,5 +1,6 @@
 const { convert, obj, type } = require("./utils");
 const { Memory } = require("./memory");
+const { globalSteps } = require("./globalSteps");
 
 function Chain(blueprint) {
   var instruct = blueprint.instruct;
@@ -26,94 +27,7 @@ function Chain(blueprint) {
 Chain.prototype._library = {
   chains: {},
   specials: ["if", "each", "setup"],
-  steps: {
-    "&": function(last) {
-      if (!this._conditions) {
-        this._addTools({
-          _conditions: [last]
-        });
-      }
-    },
-    alert: function(messageProp) {
-      var message = obj.deep(this, messageProp);
-      alert(message || messageProp);
-    },
-    each: function(notSure, next) {
-      var each = this._step.each,
-          getData = each.each,
-          iteration = each.run || each.async;
-
-      var methodForEach = (i, item, nxt) => {
-        this._remember({ i, item });
-        iteration.method(this, nxt);
-      };
-
-      getData.method(this, (data) => {
-        if (!Array.isArray(data)) {
-          console.error({ notAnArray: data });
-          return;
-        }
-
-        if (each.async) {
-          data.loop(methodForEach).then(next);
-          return;
-        }
-
-        for (var i = 0; i < data.length; i++) {
-          methodForEach(i, data[i]);
-        }
-
-        next();
-      });
-    },
-    if: function(last, next) {
-      var data = this._step.if,
-          condition = data.if || data.switch;
-
-      condition.method(this, (res) => {
-        var answer = data[res],
-          allTrue = arr => !arr.filter(item => !item).length,
-          conds = this._conditions;
-
-        if (conds) {
-          answer = data[allTrue(conds)];
-        }
-
-        delete this._conditions;
-
-        if (!answer) {
-          next();
-          return;
-        };
-
-        answer.method(this, next);
-      });
-    },
-    incr: function(prop) {
-      var counter = obj.valueTip(this, prop);
-      counter.obj[counter.prop]++;
-    },
-    isString: function(item) {
-      var constant = obj.deep(this, item);
-      this.next(typeof constant == "string");
-    },
-    log: function(messageProp) {
-      var message = obj.deep(this, messageProp);
-
-      if (!message && message != 0) message = messageProp;
-
-      console.log(message);
-    },
-    restart: function(last, next) {
-      var step = this._step,
-          restart = () => step.firstStep().method(this);
-
-      setTimeout(restart);
-    },
-    wait: function(time, next) {
-      setTimeout(next, time * 1000);
-    }
-  }
+  steps: globalSteps
 }
 
 Chain.prototype.addGlobalSteps = function(steps) {
