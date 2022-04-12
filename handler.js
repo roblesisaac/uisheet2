ca9843dc26787c3dcb61c928742bf3768dcb3a15
roblesisaac <irobles1030@gmail.com>
2022-04-12 16:19:11 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,53 +1,64 @@
 'use strict';
 
-const { Chain, convert, obj, type } = require("./scripts/chain");
+const {
+  Chain,
+  convert,
+  obj,
+  type
+} = require("./scripts/chain");
 
 Chain.prototype.addGlobalSteps({
-	has: function(props) {
-		var item = obj.deep(this, props);
-		this.next(!!item || item === 0);
-	}
+  has: function(props) {
+    var item = obj.deep(this, props);
+    this.next(!!item || item === 0);
+  }
 });
 
 const db = new Chain({
-	steps: {
-		initMongo: function() {
-			this.next({
-				message: "mongo is running",
-				path: this.path,
-				event: this.event
-			});
-		}
-	},
-	instruct: {
-		init: () => [
-			"initMongo"
-		]	
-	}
+  steps: {
+    initMongo: function() {
+      this.next({
+        message: "mongo is running",
+        path: this.path,
+        event: this.event
+      });
+    }
+  },
+  instruct: {
+    init: () => [
+      "initMongo"
+    ]
+  }
 });
 
 const handle = new Chain({
   steps: {
     respond: (last, next) => {
-			next(last);
+      next(last);
     }
   },
   instruct: {
     serve: (event) => [
-			{ event	},
-			{ path: "event.pathParameters" },
-			db.init,
-			{
-				if: { has: "path.chain" },
-				true: function() {
-					var chain = this.path.chain;
-					this.next({ chain });
-				}
-			}
+      { 
+	event,
+	path: event.pathParameters
+      },
+      db.init,
+      {
+        if: {
+          has: "path.chain"
+        },
+        true: function() {
+          var chain = this.path.chain;
+          this.next({
+            chain
+          });
+        }
+      }
     ]
   }
 });
 
 module.exports.port = async event => handle.serve(event)
-                .then(response => response)
-                .catch(e => e);
+  .then(response => response)
+  .catch(e => e);
