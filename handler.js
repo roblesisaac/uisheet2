b35afd4047558ec6996a61c9bc30edb7e8efd8ce
roblesisaac <irobles1030@gmail.com>
2022-04-11 19:13:17 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const { Chain, obj, convert, type } = require("./scripts/chain");
+const { obj, convert, type } = require("./scripts/utils");
+const { Chain } = require("./scripts/chain");
 
 Chain.prototype.addGlobalSteps({
 	has: function(props) {
