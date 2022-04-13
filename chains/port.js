15e225b8bfa87f0561c4037ee2d913d1b147b573
roblesisaac <irobles1030@gmail.com>
2022-04-12 17:04:40 -0700

    Create port.js

---
+++ chains/port.js
@@ -0,0 +1,54 @@
+const { Chain, convert, obj, type } = require("./scripts/chain");
+
+Chain.prototype.addGlobalSteps({
+  has: function(props) {
+    var item = obj.deep(this, props);
+    this.next(!!item || item === 0);
+  }
+});
+
+const db = new Chain({
+  steps: {
+    initMongo: function() {
+      this.next({
+        message: "mongo is running",
+        path: this.path,
+        event: this.event
+      });
+    }
+  },
+  instruct: {
+    init: () => [
+      "initMongo"
+    ]
+  }
+});
+
+const handle = new Chain({
+  steps: {
+    respond: (last, next) => {
+      next({ last });
+    }
+  },
+  instruct: {
+    serve: (event) => [
+      { 
+	event,
+	path: event.pathParameters
+      },
+      db.init,
+      {
+        if: {
+          has: "path.chain"
+        },
+        true: function() {
+          var chain = this.path.chain;
+          this.next({ chain, message: "working!" });
+        }
+      },
+      "respond"
+    ]
+  }
+});
+
+module.exports = { handle };
