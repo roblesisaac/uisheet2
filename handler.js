600aed64b2129e4877c7caed32f48a367f0dbedf
roblesisaac <irobles1030@gmail.com>
2022-04-12 17:07:51 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,7 +1,11 @@
 'use strict';
 
+try {
 const { port } = require("./chains/port");
 
 module.exports.port = async event => port.serve(event)
   .then(response => response)
   .catch(e => e);
+} catch (e) {
+  return { error: e.toString() }
+}
