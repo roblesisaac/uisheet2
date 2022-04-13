e71e7fefd6bc0bd5bc6bfa75ac82837aad4b5084
roblesisaac <irobles1030@gmail.com>
2022-04-12 17:05:54 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,58 +1,7 @@
 'use strict';
 
-const { Chain, convert, obj, type } = require("./scripts/chain");
+const { port } = require("./chains/port");
 
-Chain.prototype.addGlobalSteps({
-  has: function(props) {
-    var item = obj.deep(this, props);
-    this.next(!!item || item === 0);
-  }
-});
-
-const db = new Chain({
-  steps: {
-    initMongo: function() {
-      this.next({
-        message: "mongo is running",
-        path: this.path,
-        event: this.event
-      });
-    }
-  },
-  instruct: {
-    init: () => [
-      "initMongo"
-    ]
-  }
-});
-
-const handle = new Chain({
-  steps: {
-    respond: (last, next) => {
-      next({ last });
-    }
-  },
-  instruct: {
-    serve: (event) => [
-      { 
-	event,
-	path: event.pathParameters
-      },
-      db.init,
-      {
-        if: {
-          has: "path.chain"
-        },
-        true: function() {
-          var chain = this.path.chain;
-          this.next({ chain, message: "working!" });
-        }
-      },
-      "respond"
-    ]
-  }
-});
-
-module.exports.port = async event => handle.serve(event)
+module.exports.port = async event => port.serve(event)
   .then(response => response)
   .catch(e => e);
