3b55b11d9d2f81c6640e0f8d65cce5a68cae3c44
roblesisaac <irobles1030@gmail.com>
2022-04-22 16:28:25 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -4,9 +4,12 @@ try {
   
 const port = require("./peaches/port");
 
-module.exports.port = async event => await port.run(event)
-  .then(response => response)
-  .catch(e => e);
+module.exports.port = async event => {
+// await port.run(event)
+//   .then(response => response)
+//   .catch(e => e);
+  return event;
+}
   
 } catch (e) {
   
