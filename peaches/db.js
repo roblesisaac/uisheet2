01a67f98d36080b0db25a5b587b02153f1dc5c3d
roblesisaac <irobles1030@gmail.com>
2022-04-22 11:38:51 -0700

    Update db.js

--- peaches/db.js
+++ peaches/db.js
@@ -1,21 +1,40 @@
 const { Peach } = require("scripts/peach");
+const mongoose = require("mongoose");
 
 const db = new Peach({
   input: {
     time: () => Date.now()
   },
   steps: {
+    connect: function() {
+      var options = {
+        useCreateIndex: true,
+        autoIndex: true,
+        keepAlive: true
+      };
+      
+      mongoose.connect(process.env.DB, options).then(database => {
+        isConnected = database.connections[0].readyState;
+        this.next();
+      });
+    },
+    promiseResolve: function() {
+      Promise.resolve();
+    },
     initMongo: function() {
       this.next({
         message: `mongo is running ${this.sheetName} AT ${this.time}`,
-        path: this.path,
-        mongoUser: process.env.MONGO_KEY
+        path: this.path
       });
     }
   },
   instruct: {
     init: () => [
-      "initMongo"
+      {
+        if: !!isConnected,
+        true: "promiseResolve",
+        false: "connect"
+      }
     ]
   }
 });
