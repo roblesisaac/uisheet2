57a87463dfe6c2d921cf6051bdeba2ff54f015f2
roblesisaac <irobles1030@gmail.com>
2022-04-22 17:28:33 -0700

    Update db.js

--- peaches/db.js
+++ peaches/db.js
@@ -31,7 +31,7 @@ const db = new Peach({
       this.next(!!client);
     },
     fetchCollection: function() {
-      console.log({ sheetName });
+      console.log({ sheetNames });
       this.next({ message: "hi" });
       // dbPeach.collection(this.sheetName).find({}, (res) => {
       //   this.next({ res, sheetName: this.sheetName });
